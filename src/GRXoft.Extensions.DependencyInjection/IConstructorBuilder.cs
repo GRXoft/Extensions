673f04c85bd90add4f2be757808439787ead3302
GRXoft <GRXoft@users.noreply.github.com>
2020-10-10 13:13:14 +0200

    Added IConstructorBuilder abstraction

---
+++ src/GRXoft.Extensions.DependencyInjection/IConstructorBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GRXoft.Extensions.DependencyInjection
+{
+    public interface IConstructorBuilder<TService>
+    {
+        Func<IServiceProvider, TService> Build();
+
+        IConstructorBuilder<TService> Parameter<T>(Func<IServiceProvider, T> valueProvider);
+
+        IConstructorBuilder<TService> Parameter<T>(string name, Func<IServiceProvider, T> valueProvider);
+    }
+}
