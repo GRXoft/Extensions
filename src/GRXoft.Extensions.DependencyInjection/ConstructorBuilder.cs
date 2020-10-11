a5072581d8bf5dd4cfc529f6fa3070a8eb862030
GRXoft <GRXoft@users.noreply.github.com>
2020-10-11 11:21:01 +0200

    Basic implementation

--- src/GRXoft.Extensions.DependencyInjection/ConstructorBuilder.cs
+++ src/GRXoft.Extensions.DependencyInjection/ConstructorBuilder.cs
@@ -1,31 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace GRXoft.Extensions.DependencyInjection
 {
-    internal class ConstructorBuilder<TService> : IConstructorBuilder<TService>
+    public class ConstructorBuilder<TService> : IConstructorBuilder<TService>
     {
         private readonly ConstructorInfo _constructor;
         private readonly IReadOnlyList<ParameterInfo> _parameters;
-        private readonly IDictionary<string, ParameterValueProvider> _parameterValues;
+        private readonly IDictionary<string, Delegate> _parameterResolvers;
 
-        public ConstructorBuilder()
+        public ConstructorBuilder() : this(MatchConstructor())
         {
-            var type = typeof(TService);
-            if (type.IsInterface || type.IsAbstract)
-                throw new Exception(); // TODO
+        }
 
-            var ctors = type.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
-
-            if (ctors.Length == 0)
-                throw new Exception(); // TODO
-
-            if (ctors.Length > 1)
-                throw new Exception(); // TODO
-
-            _constructor = ctors[0];
+        public ConstructorBuilder(ConstructorInfo constructor)
+        {
+            _constructor = constructor ?? throw new ArgumentNullException(nameof(constructor));
             _parameters = _constructor.GetParameters();
 
             foreach (var parameter in _parameters)
@@ -34,46 +27,92 @@ namespace GRXoft.Extensions.DependencyInjection
                     throw new Exception(); // TODO
             }
 
-            _parameterValues = new Dictionary<string, ParameterValueProvider>();
+            _parameterResolvers = new Dictionary<string, Delegate>();
         }
 
+        /// <inheritdoc/>
         public Func<IServiceProvider, TService> Build()
         {
-            throw new NotImplementedException();
+            var sp = Expression.Parameter(typeof(IServiceProvider), "sp");
+            var parameters = new Expression[_parameters.Count];
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var parameter = _parameters[i];
+                if (_parameterResolvers.TryGetValue(parameter.Name, out var resolver))
+                {
+                    var del = Expression.Constant(resolver);
+                    var exp = Expression.Invoke(del, sp);
+                    parameters[i] = exp;
+                }
+                else
+                {
+                    var method = typeof(IServiceProvider).GetMethod(nameof(IServiceProvider.GetService));
+
+                    var type = Expression.Constant(parameter.ParameterType);
+                    var call = Expression.Call(sp, method, type);
+                    var convert = Expression.Convert(call, parameter.ParameterType);
+                    parameters[i] = convert;
+                }
+            }
+
+            var ctor = Expression.New(_constructor, parameters);
+            var lambda = Expression.Lambda<Func<IServiceProvider, TService>>(ctor, sp);
+
+            return lambda.Compile();
+        }
+         
+        /// <inheritdoc/>
+        public void ResolveParameter<T>(string name, Func<IServiceProvider, T> resolver, bool overwrite)
+        {
+            if (name is string && string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(); // TODO
+
+            var parameter = MatchParameter(typeof(T), name);
+
+            if (!overwrite && _parameterResolvers.ContainsKey(parameter.Name))
+                throw new Exception(); // TODO
+
+            _parameterResolvers[parameter.Name] = resolver;
         }
 
-        private ParameterInfo MatchParameter(string name)
+        private static ConstructorInfo MatchConstructor()
         {
-            var matches = _parameters.Where(p => p.Name.Equals(name, StringComparison.Ordinal)).GetEnumerator();
+            var type = typeof(TService);
+            if (type.IsInterface || type.IsAbstract)
+                throw new Exception(); // TODO
 
-            if (!matches.MoveNext())
+            var ctors = type.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+            if (ctors.Length == 0)
+                ctors = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (ctors.Length == 0)
+                throw new Exception(); // TODO
+
+            if (ctors.Length > 1)
+                throw new Exception(); // TODO
+            
+            return ctors[0];
+        }
+
+        private ParameterInfo MatchParameter(Type type, string name)
+        {
+            var matchingParameters = _parameters.Where(p => p.ParameterType.Equals(type));
+
+            if (name is string)
+                matchingParameters = matchingParameters.Where(p => p.Name.Equals(name, StringComparison.Ordinal));
+
+            var enumerator = matchingParameters.GetEnumerator();
+
+            if (!enumerator.MoveNext())
                 throw new Exception(); // TODO: No match
 
-            var match = matches.Current;
+            var matchedParameter = enumerator.Current;
 
-            if (matches.MoveNext())
+            if (enumerator.MoveNext())
                 throw new Exception(); // TODO: Ambiguous match
 
-            return match;
-        }
-
-        private ParameterInfo MatchParameter(Type type)
-        {
-            var matches = _parameters.Where(p => p.ParameterType.Equals(type)).GetEnumerator();
-
-            if (!matches.MoveNext())
-                throw new Exception(); // TODO: No match
-
-            var match = matches.Current;
-
-            if (matches.MoveNext())
-                throw new Exception(); // TODO: Ambiguous match
-
-            return match;
-        }
-
-        private abstract class ParameterValueProvider
-        {
+            return matchedParameter;
         }
     }
 }
--- src/GRXoft.Extensions.DependencyInjection/IConstructorBuilder.cs
+++ src/GRXoft.Extensions.DependencyInjection/IConstructorBuilder.cs
@@ -3,14 +3,17 @@
 namespace GRXoft.Extensions.DependencyInjection
 {
     /// <summary>
-    /// Provides means to setup construction of an instance of <typeparamref name="TService"/>.
+    /// Provides means to setup construction of instances of <typeparamref name="TService"/>.
+    /// <para>
+    /// By default, all parameters should be resolved by calling <see cref="IServiceProvider.GetService(Type)"/>.
+    /// </para>
     /// </summary>
     /// <typeparam name="TService">Constructed service type.</typeparam>
     public interface IConstructorBuilder<out TService>
     {
         /// <summary>
         /// Creates a delegate capable of instantiating <typeparamref name="TService"/>
-        /// using given <see cref="IServiceProvider"/>.
+        /// using an <see cref="IServiceProvider"/>.
         /// </summary>
         /// <exception cref="InvalidOperationException">
         /// Thrown when a construction delegate could not be created. More details
@@ -19,16 +22,20 @@ namespace GRXoft.Extensions.DependencyInjection
         Func<IServiceProvider, TService> Build();
 
         /// <summary>
-        /// Configures the parameter with given <paramref name="name"/> or type to be resolved
-        /// using given <paramref name="resolver"/>.
+        /// Configures a contructor parameter to be resolved using given <paramref name="resolver"/>.
         /// </summary>
         /// <typeparam name="T">Parameter type.</typeparam>
         /// <param name="name">
-        /// Parameter name. If <see langword="null"/>, the parameter will be
-        /// resolved by matching against <typeparamref name="T"/> instead.
+        /// Parameter name to match against.
+        /// <para/>
+        /// If <see langword="null"/>, the parameter will be matched against type <typeparamref name="T"/> instead.
         /// </param>
         /// <param name="resolver">
-        /// A delegate that would produce the parameter value using given <see cref="IServiceProvider"/>.
+        /// A delegate that would produce parameter value given an <see cref="IServiceProvider"/>.
+        /// <para/>
+        /// If <see langword="null"/>, the builder should clear existing matching parameter resolver
+        /// (if <paramref name="overwrite"/> is <see langword="true"/>) and fall back to default
+        /// parameter value resolver (by default a call to <see cref="IServiceProvider.GetService(Type)"/>).
         /// </param>
         /// <param name="overwrite">
         /// Value indicating whether existing parameter configuration should be overwritten.
@@ -46,12 +53,8 @@ namespace GRXoft.Extensions.DependencyInjection
         /// <item>There are multiple parameters matchin type <typeparamref name="T"/></item>
         /// </list>
         /// </exception>
-        /// <exception cref="ArgumentNullException">
-        /// Thrown when <paramref name="resolver"/> is <see langword="null"/>.
-        /// </exception>
         /// <exception cref="InvalidOperationException">
-        /// Thrown when <paramref name="overwrite"/> is <see langword="false"/> and a matching
-        /// parameter resolution is already configured.
+        /// Thrown when <paramref name="overwrite"/> is <see langword="false"/> and a matching parameter resolver was already configured.
         /// </exception>
         void ResolveParameter<T>(string name, Func<IServiceProvider, T> resolver, bool overwrite);
     }
