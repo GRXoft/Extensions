c7ccaa983cc6ee24c4d65ca5b4c587fb4ac06cd0
GRXoft <GRXoft@users.noreply.github.com>
2020-11-21 10:30:59 +0100

    Removed redundant IContructorBuilder interface

--- src/GRXoft.Extensions.DependencyInjection/ConstructorBuilder.cs
+++ src/GRXoft.Extensions.DependencyInjection/ConstructorBuilder.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace GRXoft.Extensions.DependencyInjection
 {
-    public sealed class ConstructorBuilder<TService> : IConstructorBuilder<TService>
+    public sealed class ConstructorBuilder<TService>
     {
         private static readonly MethodInfo _method = typeof(IServiceProvider).GetMethod(nameof(IServiceProvider.GetService));
 
@@ -32,11 +32,18 @@ namespace GRXoft.Extensions.DependencyInjection
             _parameterResolvers = new Dictionary<string, Delegate>();
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Creates a delegate capable of instantiating <typeparamref name="TService"/>
+        /// using an <see cref="IServiceProvider"/>.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when a construction delegate could not be created. More details
+        /// should be provided in <see cref="Exception.InnerException"/>.
+        /// </exception>
         public Func<IServiceProvider, TService> Build()
         {
             var serviceProvider = Expression.Parameter(typeof(IServiceProvider), "sp");
-            
+
             var parameters = new Expression[_parameters.Count];
             for (var i = 0; i < parameters.Length; i++)
             {
@@ -53,7 +60,41 @@ namespace GRXoft.Extensions.DependencyInjection
             return lambda.Compile();
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Configures a contructor parameter to be resolved using given <paramref name="resolver"/>.
+        /// </summary>
+        /// <typeparam name="T">Parameter type.</typeparam>
+        /// <param name="name">
+        /// Parameter name to match against.
+        /// <para/>
+        /// If <see langword="null"/>, the parameter will be matched against type <typeparamref name="T"/> instead.
+        /// </param>
+        /// <param name="resolver">
+        /// A delegate that would produce parameter value given an <see cref="IServiceProvider"/>.
+        /// <para/>
+        /// If <see langword="null"/>, the builder should clear existing matching parameter resolver
+        /// (if <paramref name="overwrite"/> is <see langword="true"/>) and fall back to default
+        /// parameter value resolver (by default a call to <see cref="IServiceProvider.GetService(Type)"/>).
+        /// </param>
+        /// <param name="overwrite">
+        /// Value indicating whether existing parameter configuration should be overwritten.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="name"/> was given, then the exception is thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="name"/> is an empty string or contains only whitespace</item>
+        /// <item>Parameter with given <paramref name="name"/> was not found</item>
+        /// <item>Parameter with given <paramref name="name"/> is not compatible with type <typeparamref name="T"/></item>
+        /// </list>
+        /// Otherwise the exception is thrown when:
+        /// <list type="bullet">
+        /// <item>Parameter of given type <typeparamref name="T"/> was not found</item>
+        /// <item>There are multiple parameters matchin type <typeparamref name="T"/></item>
+        /// </list>
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when <paramref name="overwrite"/> is <see langword="false"/> and a matching parameter resolver was already configured.
+        /// </exception>
         public void ResolveParameter<T>(string name, Func<IServiceProvider, T> resolver, bool overwrite)
         {
             if (name is string && string.IsNullOrWhiteSpace(name))
