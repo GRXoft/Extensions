0ee945ef6dabc93f02a1c765eb3e2c9311602b95
GRXoft <GRXoft@users.noreply.github.com>
2020-12-06 22:00:46 +0100

    Other tests for Resolve(Type, ...)

--- src/GRXoft.Extensions.DependencyInjection.Tests/ServiceFactoryBuilderTests/ResolveByType.cs
+++ src/GRXoft.Extensions.DependencyInjection.Tests/ServiceFactoryBuilderTests/ResolveByType.cs
@@ -8,7 +8,62 @@ namespace GRXoft.Extensions.DependencyInjection.ServiceFactoryBuilderTests
     internal class ResolveByType
     {
         [Test]
-        public void ThrowArgumentNullExceptionForResolver()
+        public void ShouldCorrectlyResolveParameter()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(Tuple<string>));
+
+            // Act
+            sut.Resolve(type: typeof(string), resolver: sp => "A test value");
+
+            // Assert
+            var tuple = (Tuple<string>)sut.Build().Invoke(null);
+            tuple.Item1.Should().Be("A test value");
+         }
+
+        [Test]
+        public void ShouldCorrectlyResolveReconfiguredParameter()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(Tuple<string>))
+                .Resolve(type: typeof(string), resolver: sp => "A test value");
+
+            // Act
+            sut.Resolve(type: typeof(string), resolver: sp => "Another test value", overwrite: true);
+
+            // Assert
+            var tuple = (Tuple<string>)sut.Build().Invoke(null);
+            tuple.Item1.Should().Be("Another test value");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenMultipleMatchingParameters()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(Tuple<string, string>));
+
+            // Act
+            var act = sut.Invoking(x => x.Resolve(type: typeof(string), resolver: sp => "test"));
+
+            // Assert
+            act.Should().Throw<ArgumentException>().And.Message.Should().Contain("Multiple parameters match specified criteria");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentExceptionWhenNoMatchingParameter()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(object));
+
+            // Act
+            var act = sut.Invoking(x => x.Resolve(type: typeof(string), resolver: sp => "test"));
+
+            // Assert
+            act.Should().Throw<ArgumentException>().And.Message.Should().Contain("No parameters match specified criteria");
+        }
+
+        [Test]
+        public void ShouldThrowArgumentNullExceptionForResolver()
         {
             // Arrange
             var sut = new ServiceFactoryBuilder(typeof(object));
@@ -21,16 +76,30 @@ namespace GRXoft.Extensions.DependencyInjection.ServiceFactoryBuilderTests
         }
 
         [Test]
-        public void ThrowArgumentNullExceptionForType()
+        public void ShouldThrowArgumentNullExceptionForType()
         {
             // Arrange
             var sut = new ServiceFactoryBuilder(typeof(object));
 
             // Act
-            var act = FluentActions.Invoking(() => sut.Resolve(type: null, resolver: sp => default));
+            var act = sut.Invoking(x => x.Resolve(type: null, resolver: sp => default));
 
             // Assert
             act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("type");
+        }
+
+        [Test]
+        public void ShouldThrowInvalidOperationExceptionWhenReconfiguringWithoutOverwrite()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(Tuple<string>))
+                .Resolve(type: typeof(string), resolver: sp => "text1");
+
+            // Act
+            var act = sut.Invoking(x => x.Resolve(type: typeof(string), resolver: sp => "text2", overwrite: false));
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>().And.Message.Should().Contain("already configured");
         }
     }
 }
