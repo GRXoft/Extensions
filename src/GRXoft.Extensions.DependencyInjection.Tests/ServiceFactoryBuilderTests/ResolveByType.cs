e978c4334afd9cc0483e90d1b26ae81ec00db909
GRXoft <GRXoft@live.com>
2020-12-06 15:51:56 +0100

    Few tests for Resolve(Type, ...)

---
+++ src/GRXoft.Extensions.DependencyInjection.Tests/ServiceFactoryBuilderTests/ResolveByType.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace GRXoft.Extensions.DependencyInjection.ServiceFactoryBuilderTests
+{
+    [TestFixture]
+    internal class ResolveByType
+    {
+        [Test]
+        public void ThrowArgumentNullExceptionForResolver()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(object));
+
+            // Act
+            var act = FluentActions.Invoking(() => sut.Resolve(type: typeof(object), resolver: null));
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("resolver");
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionForType()
+        {
+            // Arrange
+            var sut = new ServiceFactoryBuilder(typeof(object));
+
+            // Act
+            var act = FluentActions.Invoking(() => sut.Resolve(type: null, resolver: sp => default));
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("type");
+        }
+    }
+}
